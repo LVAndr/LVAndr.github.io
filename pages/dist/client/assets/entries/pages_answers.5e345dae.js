23dd0bc7b183bca379817483851c7a05fe629e11
LVAndr <111238066+LVAndr@users.noreply.github.com>
2023-09-23 00:09:26 +0300

    Add files via upload

---
+++ pages/dist/client/assets/entries/pages_answers.5e345dae.js
@@ -0,0 +1,121 @@
+import{i}from"../chunks/chunk-ca62dbf4.js";const e=`<main class="main page-answer">\r
+    <div class="main__content">\r
+\r
+        <section class="answers-questions">\r
+            <div class="answer__container">\r
+                <div class="answer__accordion">\r
+                    <h2 class="answer__accordion-title">Питання і відповіді</h2>\r
+                    <div class="accordion">\r
+                        <div class="accordion__box">\r
+                            <div class="accordion__header accordion-header">\r
+                                <h3 class="accordion-header__title">Питання 0</h3>\r
+                                <div class="accordion-header__arrow"></div>\r
+                            </div>\r
+                            <div class="accordion-header__content">\r
+                                <p class="answer-text">\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                </p>\r
+                                </div>\r
+                            </div>\r
+\r
+                        <div class="accordion__box">\r
+                            <div class="accordion__header accordion-header">\r
+                                <h3 class="accordion-header__title">Питання 1</h3>\r
+                                <div class="accordion-header__arrow"></div>\r
+                            </div>\r
+                            <div class="accordion-header__content">\r
+                                <p class="answer-text">\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                </p>\r
+                                </div>\r
+                            </div>\r
+\r
+                        <div class="accordion__box">\r
+                            <div class="accordion__header accordion-header">\r
+                                <h3 class="accordion-header__title">Питання 2</h3>\r
+                                <div class="accordion-header__arrow"></div>\r
+                            </div>\r
+                            <div class="accordion-header__content">\r
+                                <p class="answer-text">\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                </p>\r
+                                </div>\r
+                            </div>\r
+\r
+                        <div class="accordion__box">\r
+                            <div class="accordion__header accordion-header">\r
+                                <h3 class="accordion-header__title">Питання 3</h3>\r
+                                <div class="accordion-header__arrow"></div>\r
+                            </div>\r
+                            <div class="accordion-header__content">\r
+                                <p class="answer-text">\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                    Lorem ipsum dolor sit amet.\r
+                                </p>\r
+                                </div>\r
+                            </div>\r
+\r
+                        <div class="accordion__box">\r
+                            <div class="accordion__header accordion-header">\r
+                                <h3 class="accordion-header__title">Питання 4</h3>\r
+                                <div class="accordion-header__arrow"></div>\r
+                            </div>\r
+                            <div class="accordion-header__content">\r
+                                <p class="answer-text">\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                    Commodi eligendi ex illo illum ipsam laudantium!\r
+                                </p>\r
+                                </div>\r
+                            </div>\r
+\r
+                        <div class="accordion__box">\r
+                            <div class="accordion__header accordion-header">\r
+                                <h3 class="accordion-header__title">Питання 5</h3>\r
+                                <div class="accordion-header__arrow"></div>\r
+                            </div>\r
+                            <div class="accordion-header__content">\r
+                                <p class="answer-text">\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                    Commodi eligendi ex illo illum ipsam laudantium!\r
+                                </p>\r
+                                </div>\r
+                            </div>\r
+                        <div class="accordion__box">\r
+                            <div class="accordion__header accordion-header">\r
+                                <h3 class="accordion-header__title">Питання 6</h3>\r
+                                <div class="accordion-header__arrow"></div>\r
+                            </div>\r
+                            <div class="accordion-header__content">\r
+                                <p class="answer-text">\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic officiis quis vel. Commodi eligendi ex illo illum ipsam laudantium!\r
+                                    Commodi eligendi ex illo illum ipsam laudantium!\r
+                                </p>\r
+                                </div>\r
+                            </div>\r
+                        </div>\r
+\r
+                    </div>\r
+                </div>\r
+\r
+\r
+            </div>\r
+        </section>\r
+    </div>\r
+\r
+</main>\r
+\r
+`,o=Object.freeze(Object.defineProperty({__proto__:null,default:e},Symbol.toStringTag,{value:"Module"})),n=[{configName:"onRenderClient",codeFilePath:"/renderer/+onRenderClient.js",isPlusFile:!0,codeFileExports:i},{configName:"Page",codeFilePath:"/pages/answers/+Page.js",isPlusFile:!0,codeFileExports:o}];export{n as default};
